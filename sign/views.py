8b8d5ac7e0a475f963df04d2a67d3217aae2b7c2
Stiv800 <stpab@mail.ru>
2022-02-21 09:56:21 +0300

    second commit

--- sign/views.py
+++ sign/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import User
 from django.views.generic.edit import CreateView
 from .models import BaseRegisterForm
-# from django.shortcuts import redirect
-# from django.contrib.auth.models import Group
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
 
 class BaseRegisterView(CreateView):
     model = User
@@ -11,10 +11,10 @@ class BaseRegisterView(CreateView):
     success_url = '/'
 
 
-# @login_required
-# def upgrade_me(request):
-#     user = request.user
-#     authors_group = Group.objects.get(name='authors')
-#     if not request.user.groups.filter(name='authors').exists():
-#         authors_group.user_set.add(user)
-#     return redirect('/')
+@login_required
+def upgrade_me(request):
+    user = request.user
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        authors_group.user_set.add(user)
+    return redirect('/')
